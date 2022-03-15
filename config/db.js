24e1a5dc1752b474c6676de8eefd9e5c13105cc6
Neveon <41444780+Neveon@users.noreply.github.com>
2022-03-15 18:09:10 -0400

    Fixex mongoose connect options

--- config/db.js
+++ config/db.js
@@ -2,14 +2,17 @@ const mongoose = require("mongoose");
 const config = require("config");
 const db = config.get("mongoURI");
 
+// No need for these options according to: https://stackoverflow.com/questions/68958221/mongoparseerror-options-usecreateindex-usefindandmodify-are-not-supported
+options = {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true, // Responsible for server discovery and monitoring
+};
+
 const connectDB = async () => {
     try {
-        await mongoose.connect(db, {
-            useNewUrlParser: true,
-            useCreateIndex: true,
-            useFindAndModify: false,
-            useUnifiedTopology: true, // Responsible for server discovery and monitoring
-        });
+        await mongoose.connect(db);
 
         console.log("MongoDB Connected...");
     } catch (err) {
