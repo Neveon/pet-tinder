0af509fe515a0773ef4e41b7b2b56246992f2c54
Neveon <41444780+Neveon@users.noreply.github.com>
2022-04-03 19:54:24 -0400

    Added try-catch for any errors that come up

--- middleware/petfinderToken.js
+++ middleware/petfinderToken.js
@@ -3,26 +3,34 @@ const axios = require("axios");
 
 // export the function
 module.exports = async function (req, res, next) {
-    // Send post request to receive token response
-    const responseToken = await axios.post(
-        "https://api.petfinder.com/v2/oauth2/token",
-        new URLSearchParams({
-            grant_type: "client_credentials",
-            client_id: config.get("client_id"),
-            client_secret: config.get("client_secret"),
-        })
-    );
+    try {
+        // Send post request to receive token response
+        const responseToken = await axios.post(
+            "https://api.petfinder.com/v2/oauth2/token",
+            new URLSearchParams({
+                grant_type: "client_credentials",
+                client_id: config.get("client_id"),
+                client_secret: config.get("client_secret"),
+            })
+        );
 
-    // console.log(responseToken);
+        // console.log(responseToken);
 
-    // Place petfinder token in the request
-    req.petfinderToken = responseToken.data;
+        // Place petfinder token in the request
+        req.petfinderToken = responseToken.data;
 
-    // Check if there is no token
-    if (!req.petfinderToken.access_token) {
-        return res
-            .status(500)
-            .json({ msg: "Error with petfinder API token. Check with server middleware." });
+        // Check if there is no token
+        if (!req.petfinderToken.access_token) {
+            return res
+                .status(500)
+                .json({ msg: "Error with petfinder API token. Check with server middleware." });
+        }
+    } catch (error) {
+        console.error(JSON.stringify(error));
+        res.status(500).json({
+            msg: "ERROR - Cannot obtain petfinder access token. See below status from petfinder:",
+            error: JSON.stringify(error.message),
+        });
     }
 
     // Call next function in chain
