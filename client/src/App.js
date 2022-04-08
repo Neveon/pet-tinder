81107e2510437b87dc9a7bd05986835bd8406cad
Neveon <41444780+Neveon@users.noreply.github.com>
2022-04-08 00:12:25 -0400

    fixed bugs for react-router-dom and added comments

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, { Fragment } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+// In react-router-dom v6, Switch was replaced with Routes
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 // Components
 import Navbar from "./components/layout/Navbar";
@@ -15,10 +16,16 @@ const App = () => {
             <Fragment>
                 <Navbar />
                 <div className="container">
-                    <Switch>
-                        <Route exact path="/" component={Home} />
-                        <Route exact path="/about" component={About} />
-                    </Switch>
+                    <Routes>
+                        {/* 
+                            component was replaced with element in react-router-dom v6 
+
+                            We also must call functional components, as functions not 
+                            class components
+                        */}
+                        <Route exact path="/" element={Home()} />
+                        <Route exact path="/about" element={About()} />
+                    </Routes>
                 </div>
             </Fragment>
         </Router>
