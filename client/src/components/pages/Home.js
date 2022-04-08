7dd77d78d864eb0eaf81f2dd151931c547df2039
Neveon <41444780+Neveon@users.noreply.github.com>
2022-04-07 23:38:50 -0400

    placeholder home page added

---
+++ client/src/components/pages/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Home - where our swiping on pets begins</h1>
+        </div>
+    );
+};
+
+export default Home;
