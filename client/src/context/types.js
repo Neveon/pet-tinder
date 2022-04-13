e20b1259ef5cd1eb54eb7efae188d86e1d3b00d9
Neveon <41444780+Neveon@users.noreply.github.com>
2022-04-13 10:37:31 -0400

    setup context and global state with hardcoded data example

---
+++ client/src/context/types.js
@@ -0,0 +1,9 @@
+export const LIKE_PET = "LIKE_PET";
+export const DELETE_PET = "DELETE_PET";
+export const SET_CURRENT = "SET_CURRENT";
+export const CLEAR_CURRENT = "CLEAR_CURRENT";
+// export const UPDATE_PET = 'UPDATE_PET';
+export const FILTER_PETS = "FILTER_PETS";
+export const CLEAR_FILTER = "CLEAR_FILTER";
+export const SET_ALERT = "SET_ALERT";
+export const REMOVE_ALERT = "REMOVE_ALERT";
